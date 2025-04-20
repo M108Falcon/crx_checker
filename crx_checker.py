dcfb3b0acfa660e4952561d96696dd5b3d3f7847
M108Falcon <mridulwadhwa2000@gmail.com>
2025-04-20 16:05:59 +0530

    improved code to handle edge cases, updated deps to cleanup deprecated items

--- crx_checker.py
+++ crx_checker.py
@@ -1,51 +1,65 @@
+# -------------------------------------
+# Interpreter: Python
+# Version: 20241119.4
+# Function: Assess browser extensions for malware, risks and sbom vulnerabilities
+# Output: stdout
+# Created by M108Falcon
+# -------------------------------------
+
 import os
 import requests
-from urllib.parse import quote
+from urllib.parse import quote # url encode
 import subprocess
 import json
 
-# Check File Size
+# check file size
 def file_size(file_path):
     if os.path.isfile(file_path):
         file_info = os.stat(file_path)
         return file_info.st_size
 
-# permission checker
+# permission checker + risk analysis
 def permission_checker(retire_scan_file):
   high_risk = ["<all_urls>", "app.window.fullscreen.overrideEsc", "audioCapture", "browsingData", "content_security_policy", "contentSettings", "copresence", "debugger", "declarativeNetRequest", "declarativeWebRequest", "downloads", "downloads.open", "experimental", "hid", "history", "nativeMessaging", "pageCapture", "privacy", "proxy", "socket", "*://*/*", "tabCapture", "tabs", "unsafe-eval", "usb", "usbDevices", "videoCapture", "vpnProvider", "web_accessible_resources", "webNavigation"]
   medium_risk = ["activeTab", "bookmarks", "clipboardRead", "clipboardWrite", "contextMenus", "cookies", "desktopCapture", "downloads", "fileSystem", "fileSystem.directory", "fileSystem.retainEntries", "fileSystem.write", "fileSystem.writeDirectory", "geolocation", "identity", "identity.email", "management", "processes", "sessions", "syncFileSystem", "system.storage", "topSites", "tts", "webRequest", "webRequestBlocking"]
   low_risk = ["accessibilityFeatures.modify", "accessibilityFeatures.read", "alarms", "alwaysOnTopWindows", "app.window.alpha", "app.window.alwaysOnTop", "app.window.fullscreen", "app.window.shape", "background", "certificateProvider", "declarativeContent", "documentScan", "enterprise.deviceAttributes", "enterprise.hardwarePlatform", "enterprise.platformKeys", "externally_connectable", "fileBrowserHandler", "fileSystemProvider", "fontSettings", "gcm", "homepage_url", "idle", "mediaGalleries", "networking.config", "notifications", "overrideEscFullscreen", "platformKeys", "power", "printerProvider", "signedInDevices", "storage", "system.memory", "system.cpu", "system.display", "ttsEngine", "unlimitedStorage", "wallpaper", "webview"]
   
+  # Manifest PATH for extension
   manifest = retire_scan_file+"\\manifest.json"
   manifest_data = None
 
+  # load data from manifest
   with open(manifest, 'r') as manifest_json:
       manifest_data = json.load(manifest_json)
   manifest_json.close()
 
-  crx_permissions = manifest_data["permissions"]
-
   critical_permission, high_permission, medium_permission, low_permission = ([] for i in range(4))
-
+  
+  # check if host permissions exist in manifest
   if "host_permissions" in manifest_data: critical_permission.append("host_permission")
 
-  for permission in crx_permissions:
-      if permission in high_risk:
-          high_permission.append(permission)
-          
-      elif permission in medium_risk:
-          medium_permission.append(permission)
-          
-      elif permission in low_risk:
-          low_permission.append(permission)
+  # check if permissions exist in manifest
+  if "permissions" in manifest_data:
+    crx_permissions = manifest_data["permissions"]
+    
+    for permission in crx_permissions:
+        if permission in high_risk:
+            high_permission.append(permission)
+            
+        elif permission in medium_risk:
+            medium_permission.append(permission)
+            
+        elif permission in low_risk:
+            low_permission.append(permission)
 
   # Check for Content Security Policy if exists
   if "content_security_policy" in manifest_data: print("\nContent Security Policy: Present")
-  else: print("\nConent Security Policy: Not Present")
+  else: print("\nContent Security Policy: Not Present")
   
   # Risk Analysis Report
   print(f"\nRisk Analysis:\nCritical Risk: {', '.join(critical_permission)}\nHigh Risk: {', '.join(high_permission)}\nMedium Risk: {', '.join(medium_permission)}\nLow Risk: {', '.join(low_permission)}")
 
+# file scanner for malware via VirusTotal
 def vt_scan(upload_file, api_key):
   # request to upload file
   scan_files = { "file": (vt_crx, open(upload_file, "rb"), "application/x-zip-compressed") }
@@ -90,18 +104,19 @@ banner = """
 print(f"{banner}\nNOTE: Please only upload zip files for downloaded extensions and not any data containing company confidential information.")
 
 # reading api key
-key_file = open("", "r") # add location to your VirusTotal API Key in ""
+key_file = open("VT API Key.txt", "r")
 api_key = key_file.readline()
 key_file.close()
 
 #VT scanner vars
 vt_crx = input("Enter the name of zipped crx: ")
-crx_dir = "" # Add the location of dir where zips of crxs are located in ""
+crx_dir = "D:\\Pentest\\crx_checker\\zips"
 unzip_crx = vt_crx.replace(".zip","")
-unzip_crx_dir = "" # Add the location of dir where you wish to extract the zip files for further analysis in ""
+unzip_crx_dir = "D:\\Pentest\\crx_checker\\unzipped_crxs"
 upload_file = os.path.join(crx_dir,vt_crx)
 retire_scan_file = os.path.join(unzip_crx_dir, unzip_crx)
 
+# malware scan
 vt_result = vt_scan(upload_file, api_key)["data"]["attributes"]["stats"]
 print(f"Virustotal Scan Summary:\n{vt_result}")
 
@@ -115,5 +130,7 @@ permission_checker(retire_scan_file)
 
 #retire scan
 print("\nRunning Retire scan:")
-# uncomment below line before running the script and provide necessary params
-# subprocess.run(f"retire <--jsrepo <add location to jsrepo if retire can't download via network>> --path {retire_scan_file}", shell=True)
+subprocess.run(f"retire --path {retire_scan_file}", shell=True)
+
+# use below line if in corporate environment
+# subprocess.run(f"retire --jsrepo C:\\Users\\A0831400\\Downloads\\retire\\jsrepository-v4.json --path {retire_scan_file}", shell=True)
